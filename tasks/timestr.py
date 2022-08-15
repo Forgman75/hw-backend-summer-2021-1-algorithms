613fdbe944d596899a4e020a286f4281a54aa922
Forgman75 <agolikov2020@gmail.com>
2022-08-15 16:23:43 +0300

    Add a task timestr

--- tasks/timestr.py
+++ tasks/timestr.py
@@ -12,7 +12,28 @@ def seconds_to_str(seconds: int) -> str:
     3700 -> 01h01m40s
     93600 -> 01d02h00m00s
     """
-    raise NotImplementedError
+    minutes, sec = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    
+    days_str = f'{days:02d}d'
+    hrs_str = f'{hours:02d}h'
+    mins_str = f'{minutes:02d}m'
+    sec_str = f'{sec:02d}s'
+
+    if days:
+        return f'{days_str}{hrs_str}{mins_str}{sec_str}'
+
+    if hours:
+        return f'{hrs_str}{mins_str}{sec_str}'
+
+    if minutes:
+        return f'{mins_str}{sec_str}'
+
+    return sec_str
+
+  
+
 
 
 
